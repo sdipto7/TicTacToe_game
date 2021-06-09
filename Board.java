b177cb04ebfe41199bb555d338937f2d20aa06f7
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2021-06-09 23:15:58 +0600

    Board class, where the board is initialized and it's state is stored

---
+++ Board.java
@@ -0,0 +1,22 @@
+package TicTacToe_game;
+
+public class Board {
+
+    char[][] board = {{' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},};
+
+    public Board() {
+    }
+
+    void showBoard() {
+        for (char[] rows : board) {
+            for (char column : rows) {
+                System.out.print(column);
+            }
+            System.out.println();
+        }
+    }
+}
