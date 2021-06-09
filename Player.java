b15de2d05fee36cba42eb31d4d09c6d218743a79
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2021-06-09 23:16:38 +0600

    Player class, where the player information is stored

---
+++ Player.java
@@ -0,0 +1,16 @@
+package TicTacToe_game;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    String name;
+    char symbol;
+    ArrayList<Integer> positions;
+
+    public Player(String name, char symbol) {
+        this.positions = new ArrayList<>();
+        this.name = name;
+        this.symbol = symbol;
+    }
+}
